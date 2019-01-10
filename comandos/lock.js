f2432008e837f1a436b263b34ed10465539a3e8c
Lucas777701 <45185389+Lucas777701@users.noreply.github.com>
2019-01-10 12:13:02 -0300

    Create lock.js

---
+++ comandos/lock.js
@@ -0,0 +1,21 @@
+//lock.js:
+const Discord = require('discord.js');
+
+module.exports.run = async (Robin, message, args) => {
+ var nomeeapelido = message.guild.member(message.author.id).nickname || message.author.username
+if(!message.member.hasPermissions("MANAGE_MESSAGES")) return message.channel.send(`\`${nomeeapelido}\`, Sem permissão!`);   
+        
+if(!message.guild.me.hasPermissions("MANAGE_MESSAGES")) return message.channel.send(`\`${nomeeapelido}\`, Eu não tenho as permissões necessarias para isto.`);
+       
+let cargo = message.guild.roles.find(c => c.name === "@everyone")
+message.channel.overwritePermissions(cargo, {
+SEND_MESSAGES: false,
+
+});
+
+return message.channel.send(`\`${message.author.username}\`, o canal ${message.channel} foi trancado com sucesso!`);
+
+
+}
+
+module.exports.help = {name: "lock"}
