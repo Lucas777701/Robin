078702c59a183d9cab0ddebd873ff5cb5b2b1ae7
Lucas777701 <45185389+Lucas777701@users.noreply.github.com>
2019-01-10 11:59:56 -0300

    Update mute.js

--- comandos/mute.js
+++ comandos/mute.js
@@ -1,1 +1,41 @@
+//mute.js:
+const Discord = require('discord.js');
 
+module.exports.run = async (Robin, message , args) => {
+
+if(!message.member.hasPermissions("MANAGE_ROLES")) return message.channel.send(`\`${message.author.username}\`, Sem permissão!`);   
+
+let usuario = message.mentions.users.first();
+if(!usuario) return message.channel.send(`\`${message.author.username}\`, Você esqueçeu de **mencionar** o membro que deseja mutar!`);
+
+let razao = args.slice(1).join(" ")
+if(!razao) return message.channel.send(`\`${message.author.username}\`, Você se esqueceu de colocar a **razão**!`);
+
+let muterole = message.guild.roles.find(c => c.name === "MUTED")
+if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "MUTED",
+        color: "#8a8888",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false,
+          VIEW_CHANNEL: false
+
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+
+
+message.guild.member(usuario).addRole(muterole);
+return message.channel.send(`\`${message.author.username}\`, o usuario **${usuario.username}** foi mutado com sucesso!`);
+
+}
+
+module.exports.config = {name: "mute"}
